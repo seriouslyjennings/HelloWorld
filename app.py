d837248014eaaf3c078676257ed143911691de57
seriouslyjennings <71467919+seriouslyjennings@users.noreply.github.com>
2024-03-31 11:35:28 -0400

    Update app.py
    trying to see it

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.write("# Hello, World!")
-st.write("This is my first Streamlit app deployed via GitHub Codespaces.")
+st.write("This is my first Streamlit app deployed via GitHub Codespaces. I made a change and I can see it!")
